79c848ab5c60d57eacecffb3b5f91b1cfd8c0c4a
LSTAlex <alex35-24@mail.ru>
2022-09-22 16:12:39 +0300

    Line tracing for Bullet

--- Source/Shooter_Project/ShooterCharacter.cpp
+++ Source/Shooter_Project/ShooterCharacter.cpp
@@ -9,6 +9,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Sound/SoundCue.h"
 #include "Engine/SkeletalMeshSocket.h"
+#include "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -115,6 +116,23 @@ void AShooterCharacter::FireWeapon()
 		if (MuzzelFlash)
 		{
 			UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), MuzzelFlash, SocketTransform);
+		}
+
+		FHitResult FireHit;
+		const FVector Start{ SocketTransform.GetLocation()};
+		const FQuat Rotation{ SocketTransform.GetRotation() };
+		const FVector RotationAxis{Rotation.GetAxisX()};
+		const FVector End{ Start + RotationAxis * 50'000.f };
+
+		GetWorld()->LineTraceSingleByChannel(FireHit, 
+			Start,
+			End,
+			ECollisionChannel::ECC_Visibility);
+
+		if (FireHit.bBlockingHit)
+		{
+			DrawDebugLine(GetWorld(),Start,End,FColor::Red,false,1.f);
+			DrawDebugPoint(GetWorld(), FireHit.Location, 5.f, FColor::Red, false, 1.f);
 		}
 	}
 
