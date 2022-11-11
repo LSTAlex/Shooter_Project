0b34d825a400833c88229b3c2b00da12859189c9
LSTAlex <alex35-24@mail.ru>
2022-11-11 17:59:31 +0300

    Fix BaseWeaponBP with PickupWidget

---
+++ Source/Shooter_Project/Item.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class SHOOTER_PROJECT_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	//Скелетный меш для предмета
+	//Skeletal mesh for the item
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class USkeletalMeshComponent* ItemMesh;
+
+	//Линейная трассировка пересекает CollisionBox что бы показывать виджеты HUD
+	//Line trace collides wuth box to show HUD widgets
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+	//Виджет всплывающего окна, для того случая, когда игрок смотри на предмет
+	//Popup widget for when the player looks at the item
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
+
+public:
+
+};
