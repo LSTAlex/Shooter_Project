d3bb3d7b468c6723960b4e9072e9ceb7d23946c7
LSTAlex <alex35-24@mail.ru>
2022-11-15 10:59:32 +0300

    Add enum EItemState and setup states EquipWeapon

--- Source/Shooter_Project/ShooterCharacter.cpp
+++ Source/Shooter_Project/ShooterCharacter.cpp
@@ -532,6 +532,7 @@ void AShooterCharacter::EquipWeapon(AWeapon* WeaponToEquip)
 			HandSocket->AttachActor(WeaponToEquip, GetMesh());
 		}
 		EquippedWeapon = WeaponToEquip;
+		EquippedWeapon->SetItemState(EItemState::EIS_Equipped);
 	}
 }
 
